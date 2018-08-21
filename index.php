ce15ca1af73fad5ba2ce3eb77c3b56c9b2eda340
Geoffrey28 <geoffreygirard28@gmail.com>
2018-08-21 18:56:54 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Authenticate</title>
+</head>
+<body>
+    <a href="register.php">Register</a><br>
+    <a href="login.php">Login</a>  
+</body>
+</html>
